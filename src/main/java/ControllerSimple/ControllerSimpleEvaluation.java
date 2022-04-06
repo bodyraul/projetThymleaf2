820178273f8d52d20485ef9db13305ed31615052
bodyraul <aurelien.peria@gmail.com>
2022-04-06 17:01:27 +0200

    first commit

---
+++ src/main/java/ControllerSimple/ControllerSimpleEvaluation.java
@@ -0,0 +1,21 @@
+package ControllerSimple;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+@Controller
+public class ControllerSimpleEvaluation {
+
+	@GetMapping("/accueil")
+	public String welcome(Model m) {
+		m.addAttribute("message", "bonjour");
+		return ("index");
+	}
+	
+	
+	
+}
